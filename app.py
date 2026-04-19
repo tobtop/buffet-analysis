0954a6363549d5a823330cf9315f293e6c81200a
tobtop <147807043+tobtop@users.noreply.github.com>
2026-04-20 01:08:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 st.title("Busy Buffet Analysis")
 
-df = pd.read_excel("2026_Data_Test1_Final_-_Busy_Buffet_Dataset.xlsx", sheet_name="133")
+df = pd.read_excel("2026-Data-Test1-Final.xlsx", sheet_name="133")
 st.dataframe(df)
